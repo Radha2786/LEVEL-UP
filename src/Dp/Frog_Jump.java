35c2c9cc647bb0db6e1f039458a1fe3b09841312
Radha <radhasharma2786@gmail.com>
2023-12-25 22:20:08 +0530

    Frog-Jump(Code-studio)

---
+++ src/Dp/Frog_Jump.java
@@ -0,0 +1,39 @@
+package Dp;
+
+import java.util.* ;
+import java.io.*;
+public class Frog_Jump {
+    public static int frogJump(int n, int heights[]) {
+
+        // Write your code here..
+        while(n-->0){
+            int[] dp = new int[heights.length];
+            Arrays.fill(dp, -1);
+            return frog_jump(heights, dp, heights.length-1);
+        }
+        return 0;
+
+    }
+
+    public static int frog_jump(int heights[],int[] dp,int index){
+        if(index==0){
+            return 0;
+        }
+        if(dp[index]!=-1){
+            return dp[index];
+        }
+
+        // left call
+        int left=0;
+        int right = Integer.MAX_VALUE ;
+        left= frog_jump(heights, dp, index-1)+Math.abs(heights[index]-heights[index-1]);
+
+        // right call
+        if(index>1){
+            right= frog_jump(heights, dp, index-2)+Math.abs(heights[index]-heights[index-2]);
+        }
+        dp[index]=Math.min(left,right);
+        return Math.min(left, right);
+    }
+
+}
